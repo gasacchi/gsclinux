fdf60bf0c8c7d97975522cce8d5fdccce884dafe
Gasacchi <6asacchi@gmail.com>
2021-01-10 11:23:58 +0700

    change st colorscheme to yuno

--- .config/suckless/st-0.8.4/config.h
+++ .config/suckless/st-0.8.4/config.h
@@ -96,29 +96,54 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-  [0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-  [1] = "#cc241d", /* red     */
-  [2] = "#98971a", /* green   */
-  [3] = "#d79921", /* yellow  */
-  [4] = "#458588", /* blue    */
-  [5] = "#b16286", /* magenta */
-  [6] = "#689d6a", /* cyan    */
-  [7] = "#a89984", /* white   */
+//  [0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+//  [1] = "#cc241d", /* red     */
+//  [2] = "#98971a", /* green   */
+//  [3] = "#d79921", /* yellow  */
+//  [4] = "#458588", /* blue    */
+//  [5] = "#b16286", /* magenta */
+//  [6] = "#689d6a", /* cyan    */
+//  [7] = "#a89984", /* white   */
+//
+//  /* 8 bright colors */
+//  [8]  = "#928374", /* black   */
+//  [9]  = "#fb4934", /* red     */
+//  [10] = "#b8bb26", /* green   */
+//  [11] = "#fabd2f", /* yellow  */
+//  [12] = "#83a598", /* blue    */
+//  [13] = "#d3869b", /* magenta */
+//  [14] = "#8ec07c", /* cyan    */
+//  [15] = "#ebdbb2", /* white   */
+//
+//  [255] = 0,
+//
+// /* more colors can be added after 255 to use with DefaultXX */
+//  "#0d1117",
+
+  /* Yuno Colorscheme */
+  [0] = "#0D070D", /* Xiketic */
+  [1] = "#FA9D9D", /* SalmonPink     */
+  [2] = "#CCFA9D", /* YellowGreen   */
+  [3] = "#FACC9D", /* DeepChampagne  */
+  [4] = "#AD9DFA", /* MaxBluePurple    */
+  [5] = "#FB9EF8", /* VioletWeb */
+  [6] = "#9DFAFA", /* Cestele    */
+  [7] = "#FCCCF5", /* PinkLance   */
 
   /* 8 bright colors */
-  [8]  = "#928374", /* black   */
-  [9]  = "#fb4934", /* red     */
-  [10] = "#b8bb26", /* green   */
-  [11] = "#fabd2f", /* yellow  */
-  [12] = "#83a598", /* blue    */
-  [13] = "#d3869b", /* magenta */
-  [14] = "#8ec07c", /* cyan    */
-  [15] = "#ebdbb2", /* white   */
+  [8]  = "#1B0E1B", /* black   */ // Xiketic Light
+  [9]  = "#F98B8B", /* red     */ // Light Coral
+  [10] = "#C2F98B", /* green   */ // Inchworm
+  [11] = "#F9C28B", /* yellow  */ // Mellow Aplicot
+  [12] = "#9D8BF9", /* blue    */ // Medium Purple
+  [13] = "#FA89F6", /* magenta */ // VioletWeb
+  [14] = "#8BF9F9", /* cyan    */ // Electric Blue
+  [15] = "#FCC5F3", /* white   */ // Cotton Candy
 
   [255] = 0,
 
  /* more colors can be added after 255 to use with DefaultXX */
-  "#0d1117",
+  /* "#0d1117", */
 };
 
 
@@ -127,7 +152,7 @@ static const char *colorname[] = {
  * foreground, background, cursor, reverse cursor
  */
 unsigned int defaultfg = 15;
-unsigned int defaultbg = 256;
+unsigned int defaultbg = 0;
 static unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
 
