e736373c49acb7f85acad14dfffa2502137b971d
Gasacchi <6asacchi@gmail.com>
2021-01-12 16:00:02 +0700

    change script path

--- suckless/dwmblocks/blocks.def.h
+++ suckless/dwmblocks/blocks.def.h
@@ -3,33 +3,33 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
   /* cpu load */
-  {"   ", "~/.config/suckless/dwmblocks/scripts/cpu.sh",     1,     0},
+  {"   ", "~/xxx/.dev/gsclinux/suckless/dwmblocks/scripts/cpu.sh",     1,     0},
 
   /* watt */
-  {"", "~/.config/suckless/dwmblocks/scripts/watt.sh",     1,     0},
+  {"", "~/xxx/.dev/gsclinux/suckless/dwmblocks/scripts/watt.sh",     1,     0},
 
   /* cpu temp */
-  {" ", "~/.config/suckless/dwmblocks/scripts/temp.sh",     1,     0},
+  {" ", "~/xxx/.dev/gsclinux/suckless/dwmblocks/scripts/temp.sh",     1,     0},
   /* fan speed */
-  /* {" ", "~/.config/suckless/dwmblocks/scripts/fan.sh",     1,     0}, */
+  /* {" ", "~/xxx/.dev/gsclinux/suckless/dwmblocks/scripts/fan.sh",     1,     0}, */
 
   /* disk usage */
-  {" ", "~/.config/suckless/dwmblocks/scripts/disk.sh",     60,     0},
+  {" ", "~/xxx/.dev/gsclinux/suckless/dwmblocks/scripts/disk.sh",     60,     0},
 
   /* memory usage */
-  {" ", "~/.config/suckless/dwmblocks/scripts/memory.sh",     3,     0},
+  {" ", "~/xxx/.dev/gsclinux/suckless/dwmblocks/scripts/memory.sh",     3,     0},
 
   /* backlight */
-  /* {"ﯧ ", "~/.config/suckless/dwmblocks/scripts/backlight.sh",     0,     12}, */
+  /* {"ﯧ ", "~/xxx/.dev/gsclinux/suckless/dwmblocks/scripts/backlight.sh",     0,     12}, */
   
   /* volume level */
-  {"", "~/.config/suckless/dwmblocks/scripts/volume.sh",     0,     10},
+  {"", "~/xxx/.dev/gsclinux/suckless/dwmblocks/scripts/volume.sh",     0,     10},
 
   /* battery */
-  {"", "~/.config/suckless/dwmblocks/scripts/battery.sh",     10,     0},
+  {"", "~/xxx/.dev/gsclinux/suckless/dwmblocks/scripts/battery.sh",     10,     0},
 
   /* Date is formatted like like this: "[Mon 01-01-00 00:00] */
-  {" ", "~/.config/suckless/dwmblocks/scripts/time.sh",     60,     0},
+  {" ", "~/xxx/.dev/gsclinux/suckless/dwmblocks/scripts/time.sh",     60,     0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
