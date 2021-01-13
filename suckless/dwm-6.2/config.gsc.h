e40d63c5fffaae9d18e1cb9f537030a651007f2b
Gasacchi <6asacchi@gmail.com>
2021-01-13 11:40:39 +0700

    change map term to kitty

--- suckless/dwm-6.2/config.gsc.h
+++ suckless/dwm-6.2/config.gsc.h
@@ -90,7 +90,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg, "-nf", col_fg, "-sb", col_bg_second, "-sf", col_bg, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static Key keys[] = {
   /* modifier                     key        function        argument */
